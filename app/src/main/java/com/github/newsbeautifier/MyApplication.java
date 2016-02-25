43dde127aa5df1520f33265d5ee8a87ccdc01699
jerem <colinjeremie9@gmail.com>
2016-02-25 17:53:55 +0100

    splashscreen to load feeds

--- app/src/main/java/com/github/newsbeautifier/MyApplication.java
+++ app/src/main/java/com/github/newsbeautifier/MyApplication.java
@@ -4,14 +4,9 @@ import android.app.Application;
 
 import com.github.newsbeautifier.models.RSSFeed;
 import com.github.newsbeautifier.models.User;
-import com.github.newsbeautifier.utils.RSSParser;
-import com.github.newsbeautifier.utils.UpdateRSSFeedTask;
 import com.raizlabs.android.dbflow.config.FlowManager;
-import com.raizlabs.android.dbflow.sql.language.Select;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * * NewsBeautifier
@@ -30,27 +25,6 @@ public class MyApplication extends Application {
 
         if (mUser.getFeeds() == null){
             mUser.setFeeds(new ArrayList<RSSFeed>());
-        }
-        List<RSSFeed> feeds = new Select().from(RSSFeed.class).queryList();
-        addNewFeeds(feeds, Arrays.asList(RSSParser.RSS_FEEDS));
-
-        for (RSSFeed feed : feeds){
-            new UpdateRSSFeedTask().execute(feed);
-        }
-    }
-
-    private void addNewFeeds(List<RSSFeed> feeds, List<RSSFeed> defRssFeeds) {
-        for (RSSFeed tmp : defRssFeeds){
-            boolean added = true;
-            for (RSSFeed tmp2 : feeds){
-                if (tmp2.getUrl().equals(tmp.getUrl())){
-                    added = false;
-                    break;
-                }
-            }
-            if (added){
-                feeds.add(tmp);
-            }
         }
     }
 }
